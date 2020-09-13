ef7d7176ebca7a974c73d90f2f72703d79768342
knolte <karsten.nolte@gmail.com>
2020-09-13 12:03:37 +0200

    add react example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Button(props) {
+    const handleClick = () => props.onClickFunction(props.increment);
+    return (
+        <button onClick={handleClick}>
+            +{props.increment}
+        </button>
+    );
+}
+
+function Display(props) {
+    return (
+        <div>{props.message}</div>
+    );
+}
+
+function App() {
+    const [counter, setCounter] = useState(0);
+    const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
+    return (
+        <div>
+            <Button onClickFunction={incrementCounter} increment={1} />
+            <Button onClickFunction={incrementCounter} increment={5} />
+            <Button onClickFunction={incrementCounter} increment={10} />
+            <Button onClickFunction={incrementCounter} increment={100} />
+            <Display message={counter}/>
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementsByTagName('body')[0],
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
