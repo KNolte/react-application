32d63a3a8ea7c913c11e6b4673b03077492f0bd0
UI669330 <Ilm3b-rwe4>
2020-09-14 16:02:46 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function Button(props) {
+  const handleClick = () => props.onClickFunction(props.increment);
+  return (
+      <button onClick={handleClick}>
+        +{props.increment}
+      </button>
+  );
+}
+
+function Display(props) {
+  return (
+      <div>{props.message}</div>
+  );
+}
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
+  return (
+      <div style={{ textAlign: 'center' }}>
+        <Button onClickFunction={incrementCounter} increment={1} />
+        <Button onClickFunction={incrementCounter} increment={5} />
+        <Button onClickFunction={incrementCounter} increment={10} />
+        <Button onClickFunction={incrementCounter} increment={100} />
+        <Display message={counter}/>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-function Button(props) {
-    const handleClick = () => props.onClickFunction(props.increment);
-    return (
-        <button onClick={handleClick}>
-            +{props.increment}
-        </button>
-    );
-}
-
-function Display(props) {
-    return (
-        <div>{props.message}</div>
-    );
-}
-
-function App() {
-    const [counter, setCounter] = useState(0);
-    const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
-    return (
-        <div>
-            <Button onClickFunction={incrementCounter} increment={1} />
-            <Button onClickFunction={incrementCounter} increment={5} />
-            <Button onClickFunction={incrementCounter} increment={10} />
-            <Button onClickFunction={incrementCounter} increment={100} />
-            <Display message={counter}/>
-        </div>
-    );
-}
 
 ReactDOM.render(
     <App />,
